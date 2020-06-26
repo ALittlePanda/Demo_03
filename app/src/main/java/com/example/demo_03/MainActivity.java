d7b83c521d6c1fc9c27fb25965ed6c9d34849a26
ALittlePanda <1334863066@qq.com>
2020-06-26 11:07:57 +0800

    修改界面显示信息

--- app/src/main/java/com/example/demo_03/MainActivity.java
+++ app/src/main/java/com/example/demo_03/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.demo_03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.ScrollView;
+import android.widget.Scroller;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
